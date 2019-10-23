15db646953dd87de0cebca729e32aa593d4f4244
devjosegomez <devjosegomez@gmail.com>
2019-10-23 11:37:13 -0500

    Example using StackHeaps

--- src/com/exercise19stacksheaps/app/StacksHeaps.java
+++ src/com/exercise19stacksheaps/app/StacksHeaps.java
@@ -1,10 +1,28 @@
 package com.exercise19stacksheaps.app;
-
+import java.util.Stack;
 public class StacksHeaps {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Variable declaration
+		String cad = "Getting more profile views can help you get found for the right opportunity";
+		Stack<Character> letters = new Stack<Character>();
+		char[] lettersArray = cad.toCharArray();
+		
+		for(Character c : lettersArray) {
+			if(Character.toUpperCase(c) == 'A') {
+				letters.push(c);
+			}
+		}
+		
+		while(!letters.isEmpty()) {
+			System.out.println(letters.pop());
+		}
+		/*
+		for(Character c : lettersArray) {
+			System.out.println(letters.pop());
+		}
+		*/
+		
 	}
 
 }
